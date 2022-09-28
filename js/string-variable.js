786d0a52da019e51870e16a09ef71aeb117732b8
Cylin204 <111558289+Cylin204@users.noreply.github.com>
2022-09-28 16:20:36 +0700

    gf

---
+++ js/string-variable.js
@@ -0,0 +1,8 @@
+var username;
+var message;
+username='Molly';
+message='See our up coming range';
+var elName =document.getElementById('name');
+elName.textContent =username;
+var elName =document.getElementById('note');
+elNote.textContent=message;
